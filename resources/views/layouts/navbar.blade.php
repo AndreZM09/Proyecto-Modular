f63dba021bafa73c7a021438c1d2ab6e8bf1d01f
Diego Szr <diego.szr13@gmail.com>
2025-02-20 19:07:33 -0600

    Modificacion del login y diseños

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,22 +1,24 @@
-<!-- Barra de Navegación -->
-<nav class="navbar navbar-expand-lg navbar-light">
+<nav class="navbar navbar-expand-lg navbar-light navbar-custom">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
             <img src="{{ asset('imagenes/logo.png') }}" class="d-inline-block align-top" alt="Logo">
-            Mi Proyecto
+            Universidad de Guadalajara
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">Login</a>
-                </li>
-            </ul>
-        </div>
+
+        @if(isset($mostrarEnlaces) && $mostrarEnlaces)
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    </li>
+                </ul>
+            </div>
+        @endif
     </div>
 </nav>
