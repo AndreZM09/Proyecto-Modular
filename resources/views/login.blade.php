5fa68b35c90afd9bc8e00ffed53e8655212361b0
Diego Salazar <diego.szr13@gmail.com>
2025-03-04 10:45:04 -0600

    Prueba de commmit "Ignorar"

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Campañas de Marketing</title>
+    <title>Prueba de mi Rama</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="{{ asset('css/navbar.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/login.css') }}" rel="stylesheet" />
